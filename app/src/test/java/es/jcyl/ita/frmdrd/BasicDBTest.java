27a7a86ef68622b9bd1fec5b8cd11e6853d5b9d3
ita-riobrigu <ita-riobrigu@itacyl.es>
2019-10-17 14:10:36 +0200

    merge

--- app/src/test/java/es/jcyl/ita/frmdrd/BasicDBTest.java
+++ app/src/test/java/es/jcyl/ita/frmdrd/BasicDBTest.java
@@ -1,5 +1,6 @@
 package es.jcyl.ita.frmdrd;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import org.junit.Test;
@@ -14,11 +15,28 @@ public class BasicDBTest {
     @Test
     public void myfirstTest(){
 
-        TestDatabase db = new TestDatabase(ApplicationProvider.getApplicationContext());
-        System.out.println(db.getDatabaseName());
+//        TestDatabase db = new TestDatabase(ApplicationProvider.getApplicationContext());
+//        System.out.println(db.getDatabaseName());
 
-        SQLiteDatabase sqDb = SQLiteDatabase.openOrCreateDatabase("mynewdb.db", null);
+        SQLiteDatabase sqDb = SQLiteDatabase.openOrCreateDatabase("src/test/resourceS/ribera.sqlite", null);
         System.out.println(sqDb.getPath());
+
+        Cursor c= sqDb.rawQuery("select count(1) as numero from subparcelas", null);
+        int numparcelas= -1;
+        if (c.moveToFirst()) {
+            //Recorremos el cursor hasta que no haya más registros
+            do {
+                numparcelas= c.getInt(0);
+            } while(c.moveToNext());
+        }
+        System.out.println(String.format("Número de parcelas: %s", numparcelas));
+    }
+
+    /**
+     * Make sure there will be no problema access
+     */
+    public void testParallelApiDBAccess(){
+
     }
 
 }
