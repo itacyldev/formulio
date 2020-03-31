a09f4ea117e3d5f13565b9cb8aaac6dfb302785e
ita-ramvalja <ita-ramvalja@itacyl.es>
2020-03-31 14:07:50 +0200

    Creación del flujo reactivo por componente

---
+++ app/src/test/java/es/jcyl/ita/frmdrd/reactivity/ReactivityFlowGeneratorTest.java
@@ -0,0 +1,17 @@
+package es.jcyl.ita.frmdrd.reactivity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReactivityFlowGeneratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void generateReactivityFlow() {
+    }
+}
