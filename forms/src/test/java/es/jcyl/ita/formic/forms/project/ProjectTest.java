8bb4e3fc6ddd38d61bbe72aa2d57c43aae2f0231
Rosa María Muñiz Garcia <mungarro@itacyl.red>
2020-12-04 10:31:32 +0100

    FORMIC-64 Revisar renderizado de tabs y color pestanas

--- forms/src/test/java/es/jcyl/ita/formic/forms/project/ProjectTest.java
+++ forms/src/test/java/es/jcyl/ita/formic/forms/project/ProjectTest.java
@@ -59,7 +59,7 @@ public class ProjectTest {
         Project prj = list.get(0);
         prj.open();;
         List<ProjectResource> configFiles = prj.getConfigFiles();
-        Assert.assertEquals(7, configFiles.size());
+        Assert.assertEquals(8, configFiles.size());
 
         // check repositories are loaded
     }
