fa353216e56a84dacfc07b330c2345ff013fe87b
ita-riobrigu <ita-riobrigu@itacyl.es>
2023-03-06 19:38:49 +0100

    FORMIC-752 conditional stopper para detener procesamiento cuando se produce un error

---
+++ jobs/src/main/java/es/jcyl/ita/formic/jayjobs/jexl/helpers/StringHelper.java
@@ -0,0 +1,45 @@
+package es.jcyl.ita.formic.jayjobs.jexl.helpers;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.lang3.StringUtils;
+
+import es.jcyl.ita.formic.jayjobs.jexl.JexlExpressionException;
+import es.jcyl.ita.uqserv.jexl.JexlExpressionException;
+
+public class StringHelper {
+
+	public boolean emptyStr(String str) {
+		return StringUtils.isBlank(str);
+	}
+
+	public String escapeQuote(Object value) {
+		String str = String.valueOf(value);
+		return str.replace("\"", "\\\"");
+	}
+
+	public String decode(String str) {
+		return decode(str, StandardCharsets.UTF_8.name());
+	}
+
+	public String decode(String str, String charset) {
+		try {
+			return java.net.URLDecoder.decode(str, charset);
+		} catch (UnsupportedEncodingException e) {
+			throw new JexlExpressionException("Error while decoding string: " + str);
+		}
+	}
+
+	public String encode(String str) {
+		return encode(str, StandardCharsets.UTF_8.name());
+	}
+
+	public String encode(String str, String charset) {
+		try {
+			return java.net.URLEncoder.encode(str, charset);
+		} catch (UnsupportedEncodingException e) {
+			throw new JexlExpressionException("Error while encoding string: " + str);
+		}
+	}
+}
